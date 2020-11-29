23c202541309ed7c3c0a3c50fd5cd28d9b560fd3
mpark <mpark4656@gmail.com>
2020-11-29 15:22:23 -0500

    Added username search functionality to user rest service

--- src/main/java/com/app/rest/service/UserRestService.java
+++ src/main/java/com/app/rest/service/UserRestService.java
@@ -1,7 +1,25 @@
 package com.app.rest.service;
 
 import com.app.entities.User;
+import com.app.repositories.UserRepository;
+
+import javax.inject.Inject;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 @Path("user")
-public class UserRestService extends RestService<User> { }
+public class UserRestService extends RestService<User> {
+    @Inject
+    UserRepository userRepository;
+
+    @Path("find-by-username/{username}")
+    public Response get(@PathParam("username") String username) {
+        return Response.ok(userRepository.findByUsername(username)).build();
+    }
+
+    @Path("exists/{username}")
+    public Response exists(@PathParam("username") String username) {
+        return Response.ok(userRepository.usernameExists(username)).build();
+    }
+}
