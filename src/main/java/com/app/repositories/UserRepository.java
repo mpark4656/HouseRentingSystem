6e3632af2be827789bc70ba5cc8b7b6fbf33abd9
mpark <mpark4656@gmail.com>
2020-11-29 15:15:42 -0500

    Added functionality to search user by username and find if a username exists.

--- src/main/java/com/app/repositories/UserRepository.java
+++ src/main/java/com/app/repositories/UserRepository.java
@@ -8,6 +8,22 @@ public class UserRepository extends Repository<User> {
         return entityManager.find(User.class, id);
     }
 
+    public User findByUsername(String username) {
+        return
+            entityManager.createQuery("SELECT u FROM users u WHERE u.username = :username", User.class).
+            setParameter("username", username).
+            getSingleResult();
+    }
+
+    public boolean usernameExists(String username) {
+        List<User> users =
+            entityManager.createQuery("SELECT u FROM users u WHERE u.username = :username", User.class).
+            setParameter("username", username).
+            getResultList();
+
+        return !users.isEmpty();
+    }
+
     public List<User> list() {
         return entityManager.createQuery("SELECT u FROM users u", User.class).getResultList();
     }
