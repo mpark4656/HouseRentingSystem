69cae09ba6482e4a46fb94a4e8707948f3ac2c82
mpark <mpark4656@gmail.com>
2020-12-05 15:01:41 -0500

    Added status message in the modal body message

--- src/main/webapp/js/create-user.js
+++ src/main/webapp/js/create-user.js
@@ -15,7 +15,7 @@ document.getElementById('user-create-form').addEventListener('submit', function(
 
     xhr.onreadystatechange = function() {
         if(xhr.readyState == xhr.DONE && xhr.status == 200) {
-            $('#alert-message').text(formData.get('username') + " has been successfully created.");
+            $('#alert-message').text(formData.get('username') + " has been successfully created");
 
             $('#success-alert').show();
             $(function () {
@@ -27,6 +27,7 @@ document.getElementById('user-create-form').addEventListener('submit', function(
         }
 
         if(xhr.readyState == xhr.DONE && xhr.status != 200) {
+            $('#modal-body-message').text(xhr.statusText);
             $('#error-modal').modal('show');
         }
     };
