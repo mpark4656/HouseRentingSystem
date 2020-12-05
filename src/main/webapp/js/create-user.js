923e9f3ba3f554021647549dc0416d2fca977984
mpark <mpark4656@gmail.com>
2020-12-05 14:02:04 -0500

    Added ajax call to create a user account and show modal/alert

--- src/main/webapp/js/create-user.js
+++ src/main/webapp/js/create-user.js
@@ -1,1 +1,33 @@
-alert("test");
+// Initially hide the success-alert. This element is not to be destroyed as it is repeatedly used on the same page
+document.getElementById('success-alert').style.display = 'none';
+
+
+document.getElementById('user-create-form').addEventListener('submit', function(e) {
+    // Do not submit and cause any redirection
+    e.preventDefault();
+
+    // Try using the vanilla javascript for ajax instead of using jQuery for a practice
+    let xhr = new XMLHttpRequest();
+    let formData = new FormData(this);
+
+    xhr.open('POST', 'create-user');
+    xhr.send(formData);
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == xhr.DONE && xhr.status == 200) {
+            $('#alert-message').text(formData.get('username') + " has been successfully created.");
+
+            $('#success-alert').show();
+            $(function () {
+                var duration = 3000;
+                setTimeout(function () { $('#success-alert').hide(); }, duration);
+            });
+
+            formElement.reset();
+        }
+
+        if(xhr.readyState == xhr.DONE && xhr.status != 200) {
+            $('#error-modal').modal('show');
+        }
+    };
+});
