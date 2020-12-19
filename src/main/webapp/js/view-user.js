b0cb2c503b6ac776f3e7640f337a54a353c1e050
mpark <mpark4656@gmail.com>
2020-12-19 12:54:44 -0500

    Added event handlers for the View, Edit, and Delete

---
+++ src/main/webapp/js/view-user.js
@@ -0,0 +1,14 @@
+$('.button-view-user').click(function() {
+    $username = $(this).data('username');
+    alert($username);
+});
+
+$('.button-edit-user').click(function() {
+    $username = $(this).data('username');
+    alert($username);
+});
+
+$('.button-delete-user').click(function() {
+    $username = $(this).data('username');
+    alert($username);
+});
