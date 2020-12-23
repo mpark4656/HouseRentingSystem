c3af7be383d58ca9a26ddb05322abd0bc38e00ce
mpark <mpark4656@gmail.com>
2020-12-23 00:13:31 -0500

    Completed update-user method

--- src/main/java/com/app/rest/service/UserRestService.java
+++ src/main/java/com/app/rest/service/UserRestService.java
@@ -3,8 +3,8 @@ package com.app.rest.service;
 import com.app.entities.User;
 import com.app.repositories.UserRepository;
 import com.app.security.UserAuthentication;
-
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
@@ -35,7 +35,7 @@ public class UserRestService extends RestService<User> {
     @DELETE
     public Response deleteByUsername(@QueryParam("username") String username) {
         if(username.equals(UserAuthentication.ROOT_USERNAME)) {
-            return Response.status(403, "The default root account can't be deleted").build();
+            return Response.status(403, "The default root account can not be deleted").build();
         }
 
         User user = userRepository.deleteByUsername(username);
@@ -44,5 +44,30 @@ public class UserRestService extends RestService<User> {
         } else {
             return Response.ok(user).build();
         }
+    }
+
+    @Path("update-user")
+    @PUT
+    public Response updateUser(User user) {
+        // Don't trust the user ID coming from the client
+        // Use the username to pull the correct id.
+        // If username isn't found, reject the request call
+        if(user.getUsername().equals(UserAuthentication.ROOT_USERNAME)) {
+            return Response.status(403, "The default root account can not be changed").build();
+        }
+
+        User currentUser = userRepository.findByUsername(user.getUsername());
+
+        if(currentUser == null) {
+            return Response.status(404, "Unable to find this user").build();
+        }
+
+        currentUser.setFirstName(user.getFirstName().toUpperCase());
+        currentUser.setLastName(user.getLastName().toUpperCase());
+        currentUser.setEmailAddress(user.getEmailAddress().toUpperCase());
+        currentUser.setRoles(user.getRoles());
+        userRepository.update(currentUser);
+
+        return Response.ok(currentUser).build();
     }
 }
