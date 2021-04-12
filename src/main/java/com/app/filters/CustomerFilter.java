5136c4583fbc30b74ad0029a691940033842c2dc
mpark4656 <mpark4656@gmail.com>
2021-04-12 19:53:59 -0400

    Created customer filter

---
+++ src/main/java/com/app/filters/CustomerFilter.java
@@ -0,0 +1,44 @@
+package com.app.filters;
+
+import com.app.entities.User;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName="CustomerFilter", urlPatterns={"/customer/*"})
+public class CustomerFilter implements Filter {
+    @Override
+    public void init(FilterConfig config) throws ServletException {}
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException
+    {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        if(session == null) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+            return;
+        } else {
+            User user = (User) session.getAttribute("user");
+            if(user == null) {
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+                return;
+            } else {
+                if(!user.isCustomer()) {
+                    httpResponse.sendRedirect(httpRequest.getContextPath() + "/home");
+                    return;
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
