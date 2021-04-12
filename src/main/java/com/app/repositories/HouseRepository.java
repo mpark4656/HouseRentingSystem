28c8c58d513f93ae67a4280a726acf4d4a9cb1a9
mpark4656 <mpark4656@gmail.com>
2021-04-12 19:13:07 -0400

    Added logic for attaching an entity object

--- src/main/java/com/app/repositories/HouseRepository.java
+++ src/main/java/com/app/repositories/HouseRepository.java
@@ -4,10 +4,20 @@ import com.app.entities.House;
 import java.util.List;
 
 public class HouseRepository extends Repository<House> {
+    @Override
     public House find(int id) {
         return entityManager.find(House.class, id);
     }
 
+    @Override
+    public House attach(House house) {
+        if(house != null) {
+            return entityManager.find(House.class, house.getId());
+        }
+        return null;
+    }
+
+    @Override
     public List<House> list() {
         return entityManager.createQuery("SELECT h from houses h", House.class).getResultList();
     }
--- src/main/java/com/app/repositories/UserRepository.java
+++ src/main/java/com/app/repositories/UserRepository.java
@@ -5,38 +5,58 @@ import javax.persistence.NoResultException;
 import java.util.List;
 
 public class UserRepository extends Repository<User> {
+    @Override
     public User find(int id) {
         return entityManager.find(User.class, id);
     }
 
+    @Override
+    public List<User> list() {
+        return entityManager.createQuery("SELECT u FROM users u", User.class).getResultList();
+    }
+
+    @Override
+    public User attach(User user) {
+        if(user != null) {
+            return entityManager.find(User.class, user.getId());
+        }
+        return null;
+    }
+
     public User findByUsername(String username) {
         try {
-            return
-                entityManager.
-                createQuery("SELECT u FROM users u WHERE lower(u.username) = :username", User.class).
-                setParameter("username", username.toLowerCase()).
-                getSingleResult();
+            if(username != null) {
+                return
+                    entityManager.
+                        createQuery("SELECT u FROM users u WHERE lower(u.username) = :username", User.class).
+                        setParameter("username", username.toLowerCase()).
+                        getSingleResult();
+            } else return null;
         } catch(NoResultException e) {
             return null;
         }
     }
 
     public boolean usernameExists(String username) {
-        List<User> users =
-            entityManager.createQuery("SELECT u FROM users u WHERE lower(u.username) = :username", User.class).
-            setParameter("username", username.toLowerCase()).
-            getResultList();
+        if(username != null) {
+            List<User> users =
+                    entityManager.createQuery("SELECT u FROM users u WHERE lower(u.username) = :username", User.class).
+                            setParameter("username", username.toLowerCase()).
+                            getResultList();
 
-        return !users.isEmpty();
+            return !users.isEmpty();
+        } else return false;
     }
 
     public boolean emailExists(String email) {
-        List<User> users =
-            entityManager.createQuery("SELECT u FROM users u WHERE upper(u.emailAddress) = :email", User.class).
-                    setParameter("email", email.toUpperCase()).
-                    getResultList();
+        if(email != null) {
+            List<User> users =
+                    entityManager.createQuery("SELECT u FROM users u WHERE upper(u.emailAddress) = :email", User.class).
+                            setParameter("email", email.toUpperCase()).
+                            getResultList();
 
-        return !users.isEmpty();
+            return !users.isEmpty();
+        } else return false;
     }
 
     public User deleteByUsername(String username) {
@@ -45,9 +65,5 @@ public class UserRepository extends Repository<User> {
             entityManager.remove(user);
         }
         return user;
-    }
-
-    public List<User> list() {
-        return entityManager.createQuery("SELECT u FROM users u", User.class).getResultList();
     }
 }
